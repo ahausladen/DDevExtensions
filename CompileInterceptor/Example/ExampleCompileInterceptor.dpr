585fc9ee9a3b4a08f468cf2d4998bff7c915f4ff
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2024-11-21 21:40:26 +0100

    Fix #28: Fix example compilation errors

--- CompileInterceptor/Example/ExampleCompileInterceptor.dpr
+++ CompileInterceptor/Example/ExampleCompileInterceptor.dpr
@@ -8,13 +8,22 @@ uses
 {$R *.res}
 
 type
-  TExampleInterceptor = class(TInterfacedObject, ICompileInterceptor)
+  TExampleInterceptor = class(TInterfacedObject, ICompileInterceptor, ICompileInterceptor20)
   public
     function GetOptions: TCompileInterceptOptions; stdcall;
-    function GetVirtualFile(Filename: PAnsiChar): IVirtualStream; stdcall;
-    function AlterFile(Filename: PAnsiChar; Content: PAnsiChar;
-      FileDate: Integer; FileSize: Integer): IVirtualStream; stdcall;
+    function GetVirtualFile(Filename: PWideChar): IVirtualStream; stdcall; // deprecated 'implement OpenVirtualFile'
+    function AlterFile(Filename: PWideChar; Content: PByte; FileDate, FileSize: Integer): IVirtualStream; stdcall;
+    procedure InspectFilename(Filename: PWideChar; FileMode: TInspectFileMode); stdcall;
+    function AlterMessage(IsCompilerMessage: Boolean; var MsgKind: TMsgKind; var Code: Integer;
+      const Filename: IUnicodeString; var Line, Column: Integer; const Msg: IUnicodeString): Boolean; stdcall;
+    procedure CompileProject(ProjectFilename, UnitPaths, SourcePaths, DcuOutputDir: PWideChar;
+      IsCodeInsight: Boolean; var Cancel: Boolean); stdcall;
+    function CreateVirtualFile(Filename: PWideChar; out Stream: IVirtualOutStream): Boolean; stdcall;
+    function OpenVirtualFile(Filename: PWideChar; out Stream: IVirtualStream): Boolean; stdcall;
+    function FileNameDate(Filename: PWideChar; out AFileDate: Integer): Boolean; stdcall;
   end;
+
+  TDoneProc = procedure; stdcall;
 
 procedure Done; stdcall;
 begin
@@ -27,11 +36,11 @@ begin
 end;
 
 exports
-  EntryPoint name CompileInterceptorEntryPoint;
+  EntryPoint name 'CompileInterceptorEntryPoint';
 
 { TExampleInterceptor }
 
-function TExampleInterceptor.AlterFile(Filename, Content: PAnsiChar;
+function TExampleInterceptor.AlterFile(Filename: PWideChar; Content: PByte;
   FileDate, FileSize: Integer): IVirtualStream;
 begin
   Result := nil;
@@ -39,14 +48,44 @@ end;
 
 function TExampleInterceptor.GetOptions: TCompileInterceptOptions;
 begin
-  Result := CIO_VIRTUALFILES;
+  Result := CIO_INSPECTFILENAMES;
 end;
 
-function TExampleInterceptor.GetVirtualFile(Filename: PAnsiChar): IVirtualStream;
+function TExampleInterceptor.GetVirtualFile(Filename: PWideChar): IVirtualStream;
 begin
-  if SameText(ExtractFileName(Filename), 'test.inc') then
-    Result := T
   Result := nil;
+end;
+
+function TExampleInterceptor.AlterMessage(IsCompilerMessage: Boolean;
+  var MsgKind: TMsgKind; var Code: Integer; const Filename: IWideString;
+  var Line, Column: Integer; const Msg: IWideString): Boolean;
+begin
+  Result := False;
+end;
+
+procedure TExampleInterceptor.InspectFilename(Filename: PWideChar; FileMode: TInspectFileMode);
+begin
+  //WriteLn(Filename);
+end;
+
+procedure TExampleInterceptor.CompileProject(ProjectFilename, UnitPaths, SourcePaths, DcuOutputDir: PWideChar;
+  IsCodeInsight: Boolean; var Cancel: Boolean);
+begin
+end;
+
+function TExampleInterceptor.CreateVirtualFile(Filename: PWideChar; out Stream: IVirtualOutStream): Boolean;
+begin
+  Result := False;
+end;
+
+function TExampleInterceptor.OpenVirtualFile(Filename: PWideChar; out Stream: IVirtualStream): Boolean;
+begin
+  Result := False;
+end;
+
+function TExampleInterceptor.FileNameDate(Filename: PWideChar; out AFileDate: Integer): Boolean;
+begin
+  Result := False;
 end;
 
 begin
